ec85ba00d16d8360d23ff552e57f682d0de90b88
lysvallee <evelyne.theodose@gmail.com>
2024-08-25 22:17:22 +0200

    data auth

--- data/api/data_api.py
+++ data/api/data_api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, Depends, Request
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from sqlmodel import Session, select, col
 from typing import List
 from datetime import datetime
@@ -7,6 +8,7 @@ from services import get_db, create_db_and_tables
 import os
 import time
 import logging
+import secrets
 from models import ServiceMetrics
 from datetime import datetime
 
@@ -23,11 +25,129 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-
+# Initialize FastAPI and Basic Auth
 app = FastAPI()
+security = HTTPBasic()
+
+# A collection of usernames that should delight Simplon and their obsession with pipes;)
+users_db = {
+    "PaulPlombier": {"username": "PaulPlombier", "password": "PlomberieSimplon_03"},
+    "RobertRobinet": {"username": "RobertRobinet", "password": "PlomberieSimplon_88"},
+    "JacquesJoint": {"username": "JacquesJoint", "password": "PlomberieSimplon_48"},
+    "ClaudineCanal": {"username": "ClaudineCanal", "password": "PlomberieSimplon_01"},
+    "TiphaineTuyau": {"username": "TiphaineTuyau", "password": "PlomberieSimplon_63"},
+}
 
 
-# Log the metrics that will be sent to Grafana via the postgreSQL database
+def authenticate(credentials: HTTPBasicCredentials = Depends(security)):
+    # Check if the user exists
+    user = users_db.get(credentials.username)
+    if not user or not secrets.compare_digest(user["password"], credentials.password):
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+
+# Use a callback to trigger the creation of tables if they don't exist yet
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+    logger.debug("Creating database tables...")
+
+
+@app.get("/categories")
+def get_categories(
+    db: Session = Depends(get_db), username: str = Depends(authenticate)
+):
+    categories = db.query(Image.image_category).distinct().all()
+    logger.debug(f"Categories: {categories}")
+    return [category[0] for category in categories]
+
+
+@app.post("/usages")
+def create_usage(
+    usage: dict, db: Session = Depends(get_db), username: str = Depends(authenticate)
+):
+    new_usage = Usage(
+        selection_time=usage["selection_time"],
+        selected_category=usage["selected_category"],
+    )
+    logger.debug(f"New usage: {new_usage}")
+    db.add(new_usage)
+    db.commit()
+    db.refresh(new_usage)
+    return new_usage
+
+
+@app.get("/usages/latest")
+def get_latest_usage(
+    db: Session = Depends(get_db), username: str = Depends(authenticate)
+):
+    latest_usage = db.exec(
+        select(Usage).order_by(col(Usage.selection_time).desc())
+    ).first()
+    logger.debug(f"Get latest usage: {latest_usage}")
+    if not latest_usage:
+        raise HTTPException(status_code=404, detail="No usage found")
+    return latest_usage
+
+
+@app.put("/usages/latest")
+def update_latest_usage(
+    update_data: dict,
+    db: Session = Depends(get_db),
+    username: str = Depends(authenticate),
+):
+    latest_usage = db.exec(
+        select(Usage).order_by(col(Usage.selection_time).desc())
+    ).first()
+    if not latest_usage:
+        raise HTTPException(status_code=404, detail="No usage found")
+    for key, value in update_data.items():
+        setattr(latest_usage, key, value)
+    db.commit()
+    db.refresh(latest_usage)
+    logger.debug(f"Update latest usage: {update_data}")
+    return latest_usage
+
+
+@app.get("/images/{category}")
+def get_images_by_category(
+    category: str,
+    page: int = 1,
+    page_size: int = 21,
+    db: Session = Depends(get_db),
+    username: str = Depends(authenticate),
+):
+    try:
+        logger.debug(
+            f"Fetching images for category: {category}, page: {page}, page_size: {page_size}"
+        )
+        skip = (page - 1) * page_size
+        image_names = (
+            db.query(Image.image_name)
+            .filter(Image.image_category == category)
+            .offset(skip)
+            .limit(page_size)
+            .all()
+        )
+        logger.debug(f"Images names: {image_names}")
+        logger.debug(f"Fetched {len(image_names)} images")
+        image_paths = []
+        for image_name in image_names:
+            image_path = os.path.join(IMAGES_DIR, image_name[0])
+            image_paths.append(image_path)
+        logger.debug(f"image paths: {image_paths}")
+        return image_paths
+    except Exception as e:
+        logger.error(f"Error fetching images: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
+
+
+# Log the service metrics that will be sent to Grafana via the postgreSQL database
 def log_response_time(
     db: Session,
     service_name: str,
@@ -68,85 +188,3 @@ async def log_requests(request: Request, call_next):
         db.close()  # Ensure the database session is closed
 
     return response
-
-
-# Use a callback to trigger the creation of tables if they don't exist yet
-@app.on_event("startup")
-def on_startup():
-    create_db_and_tables()
-    logger.debug("Creating database tables...")
-
-
-@app.get("/categories")
-def get_categories(db: Session = Depends(get_db)):
-    categories = db.query(Image.image_category).distinct().all()
-    logger.debug(f"Categories: {categories}")
-    return [category[0] for category in categories]
-
-
-@app.post("/usages")
-def create_usage(usage: dict, db: Session = Depends(get_db)):
-    new_usage = Usage(
-        selection_time=usage["selection_time"],
-        selected_category=usage["selected_category"],
-    )
-    logger.debug(f"New usage: {new_usage}")
-    db.add(new_usage)
-    db.commit()
-    db.refresh(new_usage)
-    return new_usage
-
-
-@app.get("/usages/latest")
-def get_latest_usage(db: Session = Depends(get_db)):
-    latest_usage = db.exec(
-        select(Usage).order_by(col(Usage.selection_time).desc())
-    ).first()
-    logger.debug(f"Get latest usage: {latest_usage}")
-    if not latest_usage:
-        raise HTTPException(status_code=404, detail="No usage found")
-    return latest_usage
-
-
-@app.put("/usages/latest")
-def update_latest_usage(update_data: dict, db: Session = Depends(get_db)):
-    latest_usage = db.exec(
-        select(Usage).order_by(col(Usage.selection_time).desc())
-    ).first()
-    if not latest_usage:
-        raise HTTPException(status_code=404, detail="No usage found")
-    for key, value in update_data.items():
-        setattr(latest_usage, key, value)
-    db.commit()
-    db.refresh(latest_usage)
-    logger.debug(f"Update latest usage: {update_data}")
-    return latest_usage
-
-
-@app.get("/images/{category}")
-def get_images_by_category(
-    category: str, page: int = 1, page_size: int = 21, db: Session = Depends(get_db)
-):
-    try:
-        logger.debug(
-            f"Fetching images for category: {category}, page: {page}, page_size: {page_size}"
-        )
-        skip = (page - 1) * page_size
-        image_names = (
-            db.query(Image.image_name)
-            .filter(Image.image_category == category)
-            .offset(skip)
-            .limit(page_size)
-            .all()
-        )
-        logger.debug(f"Images names: {image_names}")
-        logger.debug(f"Fetched {len(image_names)} images")
-        image_paths = []
-        for image_name in image_names:
-            image_path = os.path.join(IMAGES_DIR, image_name[0])
-            image_paths.append(image_path)
-        logger.debug(f"image paths: {image_paths}")
-        return image_paths
-    except Exception as e:
-        logger.error(f"Error fetching images: {str(e)}")
-        raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
